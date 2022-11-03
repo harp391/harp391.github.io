c9bd18a1afbc29e71f620e3851ad74898ecc575b
harp391 <harpreetkaur.ranu@gmail.com>
2022-11-03 18:54:54 -0400

    adding colors

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 (function($){
     var typed = new Typed('span.txt-rotate', {
-        strings: ["I'm a Web Developer", "I'm a Software Developer"],
+        strings: ["I'm a Front-End Developer", "I'm a Software Developer"],
         typeSpeed: 100,
         backSpeed: 100,
         fadeOut: false,
